f1ce744efa642aecabbb8f3e8e49ee1d88696846
Teemu Heinämäki <teemu.heinamaki@gmail.com>
2015-10-10 00:12:21 +0300

    Added points and a counter for them

---
+++ Assets/src/Point.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Point : MonoBehaviour {
+
+    PointCounter counter;
+
+	// Use this for initialization
+	void Start () {
+        this.counter = GameObject.Find("PointCounter").GetComponent<PointCounter>();
+        Debug.Log(counter);
+        Debug.Log("Point created with counter", this.counter);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Triggered with " + other.tag);
+        if (other.tag == "Player") {
+            counter.addPoint();
+            Destroy(this.gameObject);
+        }
+    }
+}
