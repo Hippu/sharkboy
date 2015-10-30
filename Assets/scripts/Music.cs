18d13809fcbbda772af9886c9378061b23b89780
PeterDuang <duanmuyeqiao@gmail.com>
2015-10-30 15:26:26 +0200

    StartScene

---
+++ Assets/scripts/Music.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music : MonoBehaviour {
+static Music instance = null;
+
+
+	void Awake () {
+		Debug.Log ("Music Awake "+ GetInstanceID());
+		if (instance != null) {
+			Destroy (gameObject);
+			print ("Duplicated music player destroyed!");
+		}else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+	void Start () {
+		Debug.Log ("Music Start "+ GetInstanceID());
+
+	}
+	
+	void Update () {
+	
+	}
+}
