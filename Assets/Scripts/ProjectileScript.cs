707ed7a6a5268a539b2444a3fa5ce4167635b51e
Teemu Heinämäki <teemu.heinamaki@gmail.com>
2015-12-11 01:19:27 +0200

    Shooting projectiles

---
+++ Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileScript : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public GameObject innerObject;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (rb.velocity.x > -0.01f && rb.velocity.x < 0.01f && rb.velocity.y > -0.01f && rb.velocity.y < 0.01f)
+        {
+            GameObject obj =Instantiate(innerObject, transform.position, Quaternion.identity) as GameObject;
+            Destroy(this.gameObject);
+        }
+	}
+
+    public void setInnerObject(GameObject obj)
+    {
+        innerObject = obj;
+        GetComponent<SpriteRenderer>().sprite = obj.GetComponent<SpriteRenderer>().sprite;
+        this.transform.localScale = obj.GetComponent<Transform>().localScale;
+    }
+
+}
