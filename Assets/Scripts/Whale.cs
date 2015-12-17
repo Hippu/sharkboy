cfd68ecc211325034376c38a72562868c38c561c
Josi <josi.smiley@gmx.de>
2015-12-17 15:05:14 +0200

    Whale stuff

---
+++ Assets/Scripts/Whale.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Whale : MonoBehaviour {
+
+    public GameObject sign;
+    public int foodRequired;
+    public string nextLevel;
+    private int foodEaten = 0;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void onTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag != "player")
+        {
+            this.foodEaten += 1;
+            Destroy(other.gameObject);
+            if (foodEaten >= foodRequired)
+            {
+                Application.LoadLevel(nextLevel);
+            }
+        }
+    }
+}
